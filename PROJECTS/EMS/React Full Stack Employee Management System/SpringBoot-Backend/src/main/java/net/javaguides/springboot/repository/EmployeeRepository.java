180e8e7d3d47364c97ee244a029e157179fdca0e
Logesh Vasudevan <87656174+logesh26@users.noreply.github.com>
2022-11-25 15:10:41 +0530

    EMS

---
+++ PROJECTS/EMS/React Full Stack Employee Management System/SpringBoot-Backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // all crud database methods
+}
+
+
